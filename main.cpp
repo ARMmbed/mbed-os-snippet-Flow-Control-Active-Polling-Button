38456dc82fd7029f6a568a76aca4deeefbf79b0d
adbridge <anna.bridge@arm.com>
2020-05-21 17:11:23 +0100

    Mirrored from master snippet repository

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "mbed.h"
+
+DigitalIn button(BUTTON1); // Change to match your board
+DigitalOut led(LED1);
+
+#define BUTTON_PRESS 0
+
+int main()
+{
+    while (1) {
+        if (BUTTON_PRESS == button) {
+            led = !led;
+            ThisThread::sleep_for(1000);
+        }
+    }
+}
